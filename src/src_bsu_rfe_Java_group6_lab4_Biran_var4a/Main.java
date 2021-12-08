f9c2c8048f2a85fbd3cffc9b9841d9073d175309
biranartem <artem.biran@mail.ru>
2021-12-08 15:55:45 +0300

    start

---
+++ src/src_bsu_rfe_Java_group6_lab4_Biran_var4a/Main.java
@@ -0,0 +1,15 @@
+package src_bsu_rfe_Java_group6_lab4_Biran_var4a;
+
+import javax.swing.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Создать и показать экземпляр главного окна приложения
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+}
